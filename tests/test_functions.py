1669a489a88e18a198ea48a67b0b9c45eb76d249
Dmitry Zakhs <d.zakhs@gmail.com>
2023-03-02 12:18:12 +0300

    add docstrings to functions add tests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,1 +1,18 @@
-from utils import functions
+from utils.functions import load_data, actual_data, sorted_data, print_data
+import pytest
+
+def test_load_data():
+    url ="https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp=1677833602753&signature=wdPXqZwXgObQzHL6BWlTbyok6sgWwsLYF54a_6xXTLI&downloadName=operations.json"
+    assert load_data(url) is not None
+def test_actual_data(test_data):
+   data = actual_data(test_data)
+   assert len(data) == 3
+
+def test_sorted_data(test_data):
+    data = sorted_data(test_data)
+    assert data[0]['date'] == "2019-08-26T10:50:58.294041"
+    assert len(data[:2]) == 2
+
+def test_print_data(test_data):
+    data = print_data(test_data[:1])
+    assert data == ['26.08.2019 Перевод организации\nMaestro 1596 83** **** 5199 -> Счет **9589\n31957.58 руб.\n']
--- utils/functions.py
+++ utils/functions.py
@@ -2,12 +2,23 @@ import requests
 from datetime import datetime
 
 def load_data(url):
+    """
+    Функция получения списка со словарями в формате python из json файла
+    :param url: ссылка на json файл
+    :return: список со словарями
+    """
     responce = requests.get(url)
     data = responce.json()
     return data
 
 
 def actual_data(data):
+    """
+    Функция отфильтровывает данные по исполненным транзакциям и наличием данных отправителя
+    :param data: список со словарями
+    :return: список со словарями с данными отфильтрованными по наличию данных отправителя
+    и исполненными транзакциям
+    """
     actual_data = []
     for transaction in data:
         if 'state' in transaction and transaction['state'] == 'EXECUTED' and 'from' in transaction:
@@ -16,12 +27,22 @@ def actual_data(data):
 
 
 def sorted_data(data):
+    """
+    Функция сортирует данные о транзакциях по дате
+    :param data: отфильрованный список со словарями
+    :return: список со словарями содержащий 5 последних транзакций
+    """
     sorted_data = sorted(data, key=lambda x: x['date'], reverse=True)
     return sorted_data[:5]
 
 
 
 def print_data(data):
+    """
+    Функция выводит список транзакций в формате указанном в задании
+    :param data: список последних 5 транзакций
+    :return: список транзакций в формате указанном в задании
+    """
     task_data=[]
     for transaction in data:
         date = datetime.strptime(transaction['date'], '%Y-%m-%dT%H:%M:%S.%f').strftime('%d.%m.%Y')
